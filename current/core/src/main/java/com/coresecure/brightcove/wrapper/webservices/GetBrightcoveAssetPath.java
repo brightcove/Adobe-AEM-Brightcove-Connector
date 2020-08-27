df1c874f3e47f244bdb5c1f82988e36244448c2c
aragiphani406 <69352491+aragiphani406@users.noreply.github.com>
2020-08-26 22:01:22 -0400

    Added Brightcove videos to content browser

---
+++ current/core/src/main/java/com/coresecure/brightcove/wrapper/webservices/GetBrightcoveAssetPath.java
@@ -0,0 +1,107 @@
+/*
+
+    Adobe AEM Brightcove Connector
+
+    Copyright (C) 2020 3|Share Corp.
+
+    Authors:    Arvind Ragiphani
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    - Additional permission under GNU GPL version 3 section 7
+    If you modify this Program, or any covered work, by linking or combining
+    it with httpclient 4.1.3, httpcore 4.1.4, httpmine 4.1.3, jsoup 1.7.2,
+    squeakysand-commons and squeakysand-osgi (or a modified version of those
+    libraries), containing parts covered by the terms of APACHE LICENSE 2.0
+    or MIT License, the licensors of this Program grant you additional
+    permission to convey the resulting work.
+
+ */
+
+
+package com.coresecure.brightcove.wrapper.webservices;
+
+import com.coresecure.brightcove.wrapper.sling.ConfigurationGrabber;
+import com.coresecure.brightcove.wrapper.sling.ConfigurationService;
+import com.coresecure.brightcove.wrapper.sling.ServiceUtil;
+import com.coresecure.brightcove.wrapper.utils.TextUtil;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+@Component
+@Properties(value = {
+        @Property(name = "sling.servlet.extensions", value = {"json"}),
+        @Property(name = "sling.servlet.paths", value = {"/bin/brightcove/getBrightcoveAssetPath"})
+})
+public class GetBrightcoveAssetPath extends SlingAllMethodsServlet {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GetBrightcoveAssetPath.class);
+
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest request,
+                         final SlingHttpServletResponse response) throws ServletException,
+            IOException {
+        getBrightcoveAssetsPath(request, response);
+
+    }
+
+
+    public void getBrightcoveAssetsPath(final SlingHttpServletRequest request,
+                    final SlingHttpServletResponse response) throws ServletException,
+            IOException {
+        PrintWriter outWriter = response.getWriter();
+        response.setContentType("application/json");
+        JSONObject root = new JSONObject();
+
+        try {
+
+            String brcAccountId = ServiceUtil.getAccountFromCookie(request);
+            ConfigurationGrabber cg = ServiceUtil.getConfigurationGrabber();
+            ConfigurationService brcService = cg.getConfigurationService(brcAccountId);
+            
+            String assetsPath = brcService.getAssetIntegrationPath();
+            LOGGER.debug("Brightcove Videos Asset Path is {}", assetsPath);
+
+            root.put("brightcoveAssetPath", assetsPath);
+            outWriter.write(root.toString(1));
+        } catch (JSONException e) {
+            LOGGER.error("JSONException", e);
+            outWriter.write("{\"accounts\":[],\"error\":\"" + e.getMessage() + "\"}");
+        }
+    }
+
+}
---
+++ current/ui.apps/src/main/content/jcr_root/apps/brightcove/clientlibs/brightcove-asset-finder/source/js/asset_content_finder.js
@@ -0,0 +1,46 @@
+(function ($, $document, author) {
+    var self = {},
+        assetPath = null,
+        CONTENT_FINDER_NAME = 'Brightcove Videos';
+    
+    // Get Brightcove Videos Asset Path from OSGI Config
+    $.getJSON("/bin/brightcove/getBrightcoveAssetPath.json").done(function(data){
+        assetPath = data["brightcoveAssetPath"];
+        
+        getBrightcoveAssetsPath(assetPath);
+    });
+    
+    // Get Brightcove Videos 
+    function getBrightcoveAssetsPath(brightcoveVideosPath) {
+
+        var brightcoveAssetServlet = '/bin/wcm/contentfinder/asset/view.html',
+            itemResourceType = 'cq/gui/components/authoring/assetfinder/asset';
+    
+        self.loadAssets = function (query, lowerLimit, upperLimit) {
+            query = query.concat("\"jcr:content/metadata/dam:status\": \"approved\" ");
+            query = query.concat("order:\"-jcr:content/jcr:lastModified\" ");
+    
+            var param = {
+                '_dc': new Date().getTime(),
+                'query': query.concat("order:\"-jcr:content/jcr:lastModified\" "),
+                'mimeType': 'video,application/x-shockwave-flash,application/vnd.rn-realmedia,application/mxf',
+                'itemResourceType': itemResourceType,
+                'limit': lowerLimit + ".." + upperLimit,
+                '_charset_': 'utf-8'
+            };
+    
+            return $.ajax({
+                type: 'GET',
+                dataType: 'html',
+                url: Granite.HTTP.externalize(brightcoveAssetServlet) + brightcoveVideosPath,
+                data: param
+            });
+        };
+    
+        self.setSearchPath = function (spath) {
+            searchPath = spath;
+        };
+    }
+    
+    author.ui.assetFinder.register(CONTENT_FINDER_NAME, self);
+}(jQuery, jQuery(document), Granite.author));
